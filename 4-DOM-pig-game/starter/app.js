832c6801694904d7cc18aed379b68133db10ae87
rob_catlin <rob.catlin@bourne-leisure.co.uk>
2019-04-03 15:15:30 +0100

    updated functionality and stuff

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,20 +9,68 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+function changePlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
+
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6 ) + 1;
-
-// setter - how to set a value
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
-
-// getter - how to take a value
-// var x = document.querySelector('#score-0').textContent;
-// console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  // random number
+    var dice = Math.floor(Math.random() * 6 ) + 1;
+
+  //display result
+    // setter - how to set a value
+    document.querySelector('#current-' + activePlayer).textContent = dice;
+    // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
+    // getter - how to take a value
+    // var x = document.querySelector('#score-0').textContent;
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+
+  //update round score, but only if the roll wasnt a 1
+    if (dice !== 1) {
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      changePlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // add current score to global score
+    scores[activePlayer] += roundScore;
+  // update UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  // has player won the game?
+    if(scores[activePlayer] >= 20) {
+      document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+      changePlayer();
+    }
+});
