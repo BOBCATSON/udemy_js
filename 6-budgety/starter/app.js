2a6db7a5445214c227ee556651f0eed597c60b75
BOBCATSON <robjcatlin@gmail.com>
2019-08-01 13:34:41 +0100

    budgety js update

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -8,9 +8,23 @@ var budgetController = (function() {
 
 
 //UI CONTROLLER
-var UIController = (function() {
+var UIController = (function()
 
+  var DOMstrings= {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value'
+  }
 
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp.
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    }
+  }
 
 })();
 
@@ -20,11 +34,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var ctrlAddItem = function() {
     // get field input data
+    var input = UIController.getInput();
+    console.log(input);
     // add item to budget CONTROLLER
     // ADD ITEM TO User iNTERFACE
     // Calculate the budget
     // display budget on UI
-    console.log('it works');
   };
 
   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
